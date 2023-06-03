4b74b146411e32e75aadf9621838d9643d8a9e7a
SergeyFullstack <111132907+SergeyFullstack@users.noreply.github.com>
2023-06-03 19:47:59 +0300

    Create hw-03 task2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,25 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+
+import { ImageGallery } from './ImageGallery/ImageGallery';
+import { Searchbar } from './Searchbar/Searchbar';
+
+export class App extends Component {
+  state = {
+    searchName: '',
+  };
+
+  onSubmitSearch = searchName => {
+    this.setState({ searchName });
+  };
+
+  render() {
+    return (
+      <>
+        <Searchbar onSubmit={this.onSubmitSearch} />
+        {this.state.searchName && (
+          <ImageGallery searchName={this.state.searchName} />
+        )}
+      </>
+    );
+  }
+}
